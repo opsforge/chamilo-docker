1fd079f942836a6cf26908e3e5394e675a756f98
George Svachulay <opsforge-n7>
2016-10-18 00:12:11 +0100

    Initial commit with working project files
    Removed tar due to limit issues. Added startup.sh instead for quick run

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://github.com/chamilo/chamilo-lms/releases/download/v1.10.8/chamilo-lms-1.10.8.tar.gz
+tar xvf chamilo-lms-1.10.8.tar.gz
+rm chamilo-lms-1.10.8.tar.gz
+cd ./dockerimage
+docker build -t 'opsforge/camilo:latest' .
+cd ..
+docker-compose up -d
+
+echo ""
+echo "Chamilio on Docker successfully set up"
+echo ""
